dd7ae2f83930fe1906b726bd71c3faea9cc862f2
ichsanna <43778566+ichsanna@users.noreply.github.com>
2022-05-03 08:17:03 +0700

    Update string to template literal

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const projects = [
         github: "https://github.com/ichsanna/jadwalsholatkmt",
         website: "",
         tech: ["NodeJS", "ExpressJS"],
-        description: "This is my first app built using NodeJS. I built this in order to understand NodeJS, REST API " +
-            "concept, and some npm modules. This app purpose is to automate the process of parsing the xls file " +
-            "of monthly prayer schedule into text broadcast-ready format."
+        description: `This is my first app built using NodeJS. I built this in order to understand NodeJS, REST API 
+            concept, and some npm modules. This app purpose is to automate the process of parsing the xls file 
+            of monthly prayer schedule into text broadcast-ready format.`
     },
     {
         name: "QRPresence - Web",
@@ -15,10 +15,10 @@ const projects = [
         github: "https://github.com/ichsanna/QRPresence",
         website: "",
         tech: ["NodeJS", "ExpressJS", "MongoDB"],
-        description: "I developed QRPresence along with my 2 other friends as semester project in specific college " +
-            "subject. This web app provides digital attendance service using QR Code. QRPresence lets users " +
-            "create classes along with their QR Code. Then the QR Code can be scanned by the mobile app to do an " +
-            "attendance."
+        description: `I developed QRPresence along with my 2 other friends as semester project in specific college 
+            subject. This web app provides digital attendance service using QR Code. QRPresence lets users 
+            create classes along with their QR Code. Then the QR Code can be scanned by the mobile app to do an 
+            attendance.`
     },
     {
         name: "Hospital Management Information System",
@@ -26,9 +26,9 @@ const projects = [
         github: "",
         website: "",
         tech: ["NodeJS", "ExpressJS", "MongoDB"],
-        description: "I did this project as an intern at PT Nusantera Teknologi Indonesia. " +
-            "This web app purpose is to provide various hospital services flow in RS PKU Muhammadiyah Wonosobo. " +
-            "I developed the backend part of radiology module of the system."
+        description: `I did this project as an intern at PT Nusantera Teknologi Indonesia. 
+            This web app purpose is to provide various hospital services flow in RS PKU Muhammadiyah Wonosobo. 
+            I developed the backend part of radiology module of the system.`
     },
     {
         name: "Sistem Informasi GAMA Press (SIGAP)",
@@ -36,10 +36,10 @@ const projects = [
         github: "",
         website: "",
         tech: ["PHP", "CodeIgniter", "MySQL"],
-        description: "This information system is used by UGM Press to manage various business flow. I " +
-            "developed this project along with my 2 friends as a Capstone Project(Undergraduate Thesis). We added " +
-            "and developed a postproduction warehouse management system into SIGAP which consists of features " +
-            "such as book stock, book asset, book receive, book transfer, book order, and book transaction."
+        description: `This information system is used by UGM Press to manage various business flow. I 
+            developed this project along with my 2 friends as a Capstone Project(Undergraduate Thesis). We added 
+            and developed a postproduction warehouse management system into SIGAP which consists of features 
+            such as book stock, book asset, book receive, book transfer, book order, and book transaction.`
     },
     {
         name: "URL Shortener",
@@ -47,15 +47,14 @@ const projects = [
         github: "https://github.com/ichsanna/URL-Shortener",
         website: "",
         tech: ["NodeJS", "ExpressJS", "MongoDB"],
-        description: "A project i built to learn HTML & CSS as well as my NodeJS skills." +
-            "This application main purpose is to shorten long URLs and allows the user to share and manage these shortened URLs"
+        description: `A project i built to learn HTML & CSS as well as my NodeJS skills. 
+            This application main purpose is to shorten long URLs and allows the user to share and manage these shortened URLs`
     }
 ]
 
 function fetchproject() {
     projects.reverse()
     let selector = $(".projects")
-    let techs
     selector.append('<h1 class="section-name">PROJECTS</h1>')
     for (i = 0; i < projects.length; i++) {
         techs = ""
@@ -63,33 +62,30 @@ function fetchproject() {
         website = ""
         // Tech badge
         for (j = 0; j < projects[i].tech.length; j++) {
-            techs += '<span class="badge">' + projects[i].tech[j] + '</span>'
+            techs += `<span class="badge">${projects[i].tech[j]}</span>`
         }
         // Display github link
         if (projects[i].github) {
-            github = '<a href="' + projects[i].github + '" target="_blank">' +
-                '<i class="fa-brands fa-github fa-2x"></i>' +
-                '</a>'
+            github = `<a href="${projects[i].github}" target="_blank">
+                <i class="fa-brands fa-github fa-2x"></i></a>`
         }
         // Display website link
         if (projects[i].website) {
-            website = '<a href="' + projects[i].website + '" target="_blank">' +
-                '<i class="fa-solid fa-up-right-from-square fa-2x"></i>' +
-                '</a>'
+            website = `<a href="${projects[i].website}" target="_blank">
+                <i class="fa-solid fa-up-right-from-square fa-2x"></i></a>`
         }
         // Display project name & description
         selector.append(
-            '<div class="container">' +
-            '<h3 class="project-name">' + projects[i].name + '</h3>' +
-            '<p class="project-desc">' + projects[i].description +
-            '</p>' +
-            '<div class="container d-flex flex-wrap align-content-start project-badges">' +
-            '<span class="badge project-year">' + projects[i].year + '</span>' +
-            github + website +
-            '<span class="vr"></span>' +
-            techs +
-            '</div>' +
-            '</div>'
+            `<div class="container">
+            <h3 class="project-name">${projects[i].name}</h3>
+            <p class="project-desc">${projects[i].description}</p>
+            <div class="container d-flex flex-wrap align-content-start project-badges">
+            <span class="badge project-year">${projects[i].year}</span>
+            ${github} ${website}
+            <span class="vr"></span>
+            ${techs}
+            </div>
+            </div>`
         )
 
     }
